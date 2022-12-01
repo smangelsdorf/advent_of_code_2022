9b454edab96a25715493c524b8dcb018749812c6
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2022-12-01 15:29:39 +1000

    Day 1, part 1

---
+++ day1/src/lib.rs
@@ -0,0 +1,65 @@
+enum Input {
+    Number(u64),
+    Blank,
+}
+
+#[derive(Debug)]
+enum Error {
+    IoError(std::io::Error),
+    InvalidInput,
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Error {
+        Error::IoError(e)
+    }
+}
+
+#[derive(Default)]
+struct State {
+    max: u64,
+    acc: u64,
+}
+
+impl State {
+    fn add(self, n: u64) -> State {
+        let State { acc, .. } = self;
+        State {
+            acc: acc + n,
+            ..self
+        }
+    }
+
+    fn next(self) -> State {
+        let State { max, acc } = self;
+        State {
+            max: max.max(acc),
+            acc: 0,
+        }
+    }
+}
+
+pub fn run() {
+    let r: Result<_, Error> = std::io::stdin()
+        .lines()
+        .try_fold(State::default(), |state, item| {
+            let s = item?;
+            match parse(&s)? {
+                Input::Number(n) => Ok(state.add(n)),
+                Input::Blank => Ok(state.next()),
+            }
+        });
+
+    let State { max, .. } = r.expect("Processing input").next();
+    println!("{}", max);
+}
+
+fn parse(line: &str) -> Result<Input, Error> {
+    if line == "" {
+        Ok(Input::Blank)
+    } else if let Ok(n) = u64::from_str_radix(line, 10) {
+        Ok(Input::Number(n))
+    } else {
+        Err(Error::InvalidInput)
+    }
+}
