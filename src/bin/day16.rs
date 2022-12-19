87f8ef4d19185304954f19e311e0f5156b1e4081
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2022-12-19 20:50:15 +1000

    Refine before attempting part 2

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -2,7 +2,7 @@ use id_arena::Id;
 use im::HashSet as ImHashSet;
 
 use aoc::{parser::read_from_stdin_and_parse, MoreIter};
-use graph::{ValveGraph, ValveNode};
+use graph::{ValveGraph, ValveNode, ValveNodeConnection};
 
 #[derive(Debug, PartialEq, Clone)]
 struct Valve {
@@ -12,33 +12,92 @@ struct Valve {
 
 const START_VALVE: &'static str = "AA";
 const TIME_LIMIT: u64 = 30;
+const ACTOR_COUNT: usize = 1;
 
 #[derive(Debug)]
-struct State<'a> {
-    node: &'a ValveNode,
+struct State<'a, const N: usize> {
+    actors: [(u64, &'a ValveNode); N],
     flow_rate: u64,
     released: u64,
-    time_spent: u64,
+    time_tallied: u64,
     visited: ImHashSet<Id<ValveNode>>,
 }
 
-impl<'a> State<'a> {
-    fn new(node: &'a ValveNode) -> State<'a> {
+impl<'a, const N: usize> State<'a, N> {
+    fn new(node: &'a ValveNode) -> State<'a, N> {
         State {
-            node,
+            actors: [(0, node); N],
             flow_rate: 0,
             released: 0,
-            time_spent: 0,
+            time_tallied: 0,
             visited: ImHashSet::unit(node.id),
         }
     }
 
-    fn is_valid(&self) -> bool {
-        self.time_spent < TIME_LIMIT
+    fn can_continue(&self) -> bool {
+        self.actors.iter().any(|(t, _)| t < &TIME_LIMIT)
     }
 
-    fn is_complete(&self) -> bool {
-        self.time_spent == TIME_LIMIT
+    fn is_valid(&self) -> bool {
+        self.actors.iter().all(|(t, _)| t <= &TIME_LIMIT) && self.time_tallied == TIME_LIMIT
+    }
+
+    // Compute the final total for the `State`, assuming it doesn't move again.
+    fn finish(&self) -> State<'a, N> {
+        let &State {
+            actors,
+            mut flow_rate,
+            mut released,
+            mut time_tallied,
+            ref visited,
+        } = self;
+
+        let mut actors = actors.clone();
+
+        // Accrue for visited valves that haven't been added yet.
+        for (t, node) in actors.iter_mut().filter(|(t, _)| t < &TIME_LIMIT) {
+            let t = std::mem::replace(t, TIME_LIMIT);
+            let rate = flow_rate;
+            flow_rate += node.valve.flow_rate;
+            released += (t - time_tallied) * rate;
+            time_tallied = t;
+        }
+
+        released += (TIME_LIMIT - time_tallied) * flow_rate;
+
+        State {
+            actors,
+            flow_rate,
+            released,
+            time_tallied: TIME_LIMIT,
+            visited: visited.clone(),
+        }
+    }
+
+    fn update(&self, conn: &ValveNodeConnection, new_node: &'a ValveNode) -> State<'a, N> {
+        let &State {
+            actors,
+            flow_rate,
+            released,
+            time_tallied,
+            ref visited,
+        } = self;
+
+        let release_steps = actors[0].0 - time_tallied;
+
+        let mut actors = actors.clone();
+        let new_time_spent = actors[0].0 + conn.cost + 1;
+        let (_t, old_node) = std::mem::replace(&mut actors[0], (new_time_spent, new_node));
+
+        actors.sort_by_key(|(a, _b)| *a);
+
+        State {
+            actors,
+            flow_rate: flow_rate + old_node.valve.flow_rate,
+            released: released + release_steps * flow_rate,
+            time_tallied: time_tallied + release_steps,
+            visited: visited.update(new_node.id),
+        }
     }
 }
 
@@ -46,60 +105,45 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let graph = read_from_stdin_and_parse(parser::parse_input)?;
 
     let mut terminal_states = Vec::with_capacity(1024);
-    let mut current = vec![State::new(graph.start())];
+    let mut current = vec![State::<ACTOR_COUNT>::new(graph.start())];
     while !current.is_empty() {
-        let (next, invalid) = current
+        let (next, done) = current
             .into_iter()
             .flat_map(|state| {
-                let State {
-                    node,
-                    flow_rate,
-                    released,
-                    time_spent,
-                    visited,
-                } = state;
-
                 let graph = &graph;
+                let done = state.finish();
 
-                // The result of idling here until the time expires.
-                let remaining_time = TIME_LIMIT - time_spent;
-                let done = State {
-                    node,
-                    flow_rate,
-                    released: released + remaining_time * flow_rate,
-                    time_spent: TIME_LIMIT,
-                    visited: visited.clone(),
-                };
+                let (time_spent, node) = state.actors[0];
+                let visited = state.visited.clone();
 
                 node.connections
                     .iter()
                     .filter_map(move |conn| {
                         let node = graph.get(conn.target);
-                        if visited.contains(&node.id) {
+                        if visited.contains(&node.id) || time_spent + conn.cost + 1 > TIME_LIMIT {
                             None
                         } else {
-                            Some(State {
-                                node,
-                                flow_rate: flow_rate + node.valve.flow_rate,
-                                released: released + conn.cost * flow_rate + flow_rate,
-                                time_spent: time_spent + conn.cost + 1,
-                                visited: visited.update(node.id),
-                            })
+                            Some(state.update(conn, node))
                         }
                     })
                     .once_if_empty(done)
             })
-            .partition::<Vec<_>, _>(State::is_valid);
+            .partition::<Vec<_>, _>(State::can_continue);
 
-        terminal_states.extend(invalid.into_iter().filter(State::is_complete));
-
+        terminal_states.extend(
+            done.iter()
+                .map(State::finish)
+                .filter(State::is_valid)
+                .max_by_key(|s| s.released),
+        );
         current = next;
     }
 
     let n = terminal_states
         .into_iter()
         .max_by_key(|s| s.released)
-        .unwrap();
+        .unwrap()
+        .released;
 
     println!("{:?}", n);
 
@@ -315,5 +359,148 @@ mod parser {
 
             assert_eq!(remaining_input, "");
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::parse_input;
+
+    use super::*;
+
+    #[test]
+    fn test_state_update_1() {
+        let input = "Valve AA has flow rate=0; tunnels lead to valves DD, II\n\
+                     Valve DD has flow rate=20; tunnels lead to valves AA\n\
+                     Valve II has flow rate=0; tunnels lead to valves AA, JJ\n\
+                     Valve JJ has flow rate=21; tunnel leads to valve II, KK\n\
+                     Valve KK has flow rate=5; tunnel leads to valve JJ";
+
+        let (_, graph) = parse_input(input).unwrap();
+
+        let state = State::<1>::new(graph.start());
+
+        fn f<'a>(
+            graph: &'a ValveGraph,
+            node: &'a ValveNode,
+            name: &str,
+        ) -> (&'a ValveNodeConnection, &'a ValveNode) {
+            node.connections
+                .iter()
+                .find_map(|c| {
+                    let node = graph.get(c.target);
+                    if node.valve.name == name {
+                        Some((c, node))
+                    } else {
+                        None
+                    }
+                })
+                .unwrap()
+        }
+
+        let (dd_conn, dd_node) = f(&graph, graph.start(), "DD");
+
+        let state = state.update(dd_conn, dd_node);
+        assert_eq!(
+            state
+                .actors
+                .iter()
+                .map(|(t, c)| (*t, c.id))
+                .collect::<Vec<_>>(),
+            vec![(2u64, dd_node.id)]
+        );
+
+        assert_eq!(state.flow_rate, 0);
+        assert_eq!(state.released, 0);
+        assert_eq!(state.time_tallied, 0);
+        assert_eq!(
+            state.visited,
+            [graph.start().id, dd_node.id].into_iter().collect()
+        );
+
+        let (jj_conn, jj_node) = f(&graph, dd_node, "JJ");
+
+        let state = state.update(jj_conn, jj_node);
+        assert_eq!(
+            state
+                .actors
+                .iter()
+                .map(|(t, c)| (*t, c.id))
+                .collect::<Vec<_>>(),
+            vec![(6u64, jj_node.id)]
+        );
+
+        assert_eq!(state.flow_rate, 20);
+        assert_eq!(state.released, 0);
+        assert_eq!(state.time_tallied, 2);
+        assert_eq!(
+            state.visited,
+            [graph.start().id, dd_node.id, jj_node.id]
+                .into_iter()
+                .collect()
+        );
+
+        let (kk_conn, kk_node) = f(&graph, jj_node, "KK");
+
+        let state = state.update(kk_conn, kk_node);
+        assert_eq!(
+            state
+                .actors
+                .iter()
+                .map(|(t, c)| (*t, c.id))
+                .collect::<Vec<_>>(),
+            vec![(8u64, kk_node.id)]
+        );
+
+        assert_eq!(state.flow_rate, 41);
+        assert_eq!(state.released, 80);
+        assert_eq!(state.time_tallied, 6);
+        assert_eq!(
+            state.visited,
+            [graph.start().id, dd_node.id, jj_node.id, kk_node.id]
+                .into_iter()
+                .collect()
+        );
+
+        let state = state.finish();
+
+        assert_eq!(
+            state
+                .actors
+                .iter()
+                .map(|(t, c)| (*t, c.id))
+                .collect::<Vec<_>>(),
+            vec![(TIME_LIMIT, kk_node.id)]
+        );
+
+        assert_eq!(state.flow_rate, 46);
+        assert_eq!(state.released, 162 + (TIME_LIMIT - 8) * 46);
+        assert_eq!(state.time_tallied, TIME_LIMIT);
+        assert_eq!(
+            state.visited,
+            [graph.start().id, dd_node.id, jj_node.id, kk_node.id]
+                .into_iter()
+                .collect()
+        );
+
+        let state_idle_again = state.finish();
+
+        assert_eq!(
+            state
+                .actors
+                .iter()
+                .map(|(t, c)| (*t, c.id))
+                .collect::<Vec<_>>(),
+            state_idle_again
+                .actors
+                .iter()
+                .map(|(t, c)| (*t, c.id))
+                .collect::<Vec<_>>(),
+        );
+
+        assert_eq!(state.flow_rate, state_idle_again.flow_rate);
+        assert_eq!(state.released, state_idle_again.released);
+        assert_eq!(state.time_tallied, state_idle_again.time_tallied);
+        assert_eq!(state.visited, state_idle_again.visited);
     }
 }
